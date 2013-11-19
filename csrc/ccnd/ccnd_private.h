da6e18137bbc97950901a4f7e8fa044ccec73565
chenatu <chenatu2006@gmail.com>
2013-11-19 17:31:56 +0800

    remove ccnd_msg and add bufferhead

--- csrc/ccnd/ccnd_private.h
+++ csrc/ccnd/ccnd_private.h
@@ -249,6 +249,7 @@ struct face {
     unsigned short adjstate;    /**< state of adjacency negotiotiation */
 	char* eth;
 	pcap_t* pcap_handle;
+	char* bufferhead;	/**< bufferhead include MAC address and protocol type*/
 };
 
 /** face flags */
